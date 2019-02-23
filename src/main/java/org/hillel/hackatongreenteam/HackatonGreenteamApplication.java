8f45d3db317d0ae7fccdd0ad9869463ddc86fc36
Yorix <yorix82@gmail.com>
2019-02-23 12:02:28 +0200

    Initial commit

---
+++ src/main/java/org/hillel/hackatongreenteam/HackatonGreenteamApplication.java
@@ -0,0 +1,13 @@
+package org.hillel.hackatongreenteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonGreenteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonGreenteamApplication.class, args);
+    }
+
+}
