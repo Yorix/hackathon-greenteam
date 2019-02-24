59fb6c310c09664ce51490229ec36ddc1739052f
zan-od <anatoly.zlatov@gmail.com>
2019-02-24 03:46:36 +0200

    del

--- src/main/java/org/hillel/hackatongreenteam/secutity/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package org.hillel.hackatongreenteam.secutity;
-
-public interface SecurityService {
-
-    String findLoggedInUsername();
-
-    void autoLogin(String username, String password);
-
-}
